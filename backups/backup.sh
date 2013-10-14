81c959139b9a7aebf1c56e13def6f3f39188c873
Sam Wilson <kahn@the-mesh.org>
2013-10-14 20:59:01 +1000

    Support retention and rotation

--- backups/backup.sh
+++ backups/backup.sh
@@ -2,7 +2,7 @@
 #
 # GPG encrypted backups using tar for the-mesh.org hosts
 #
-# 13 Oct 2013 Sam Wilson <kahn@the-mesh.org>
+# 14 Oct 2013 Sam Wilson <kahn@the-mesh.org>
 #
 # Setup
 # 1) As root create a new keypair
@@ -22,41 +22,93 @@
 # 2) Make sure your GPG passphrase is on the first line in /root/.backups.conf and chmod 0400
 # password
 # 3) Include your MySQL credentials in the global MySQL config
-# echo "!include /root/.my.cnf" >> /etc/my.cnf
+# echo "\!include /root/.my.cnf" >> /etc/my.cnf
 # 4) Run via cron!
-# 24 10 * * * root /root/backup.sh
+# @daily root /root/backup.sh daily
+# @weekly root /root/backup.sh weekly
+# @monthly root /root/backup.sh monthly
  
-date=`date +%F-%H`
+# Config
+backupdir=~
+remotedir=`hostname --short`
+backuphost='offsite@rsync.net'
+backupkey='-i /root/.ssh/id_rsa'
 # Specify a database name or -A for all (includes mysql table)
 database="-A"
- 
-# Bash debugging
+# Retention in days. You do the math
+daily=7
+weekly=30
+monthly=90
+# Get todays date
+date=`date +%s`
+# Debug
 #set -x
 
-# Backup databases
-cd /root/
-/usr/bin/mysqldump -u root $database > $date.sql
-/bin/tar -cJf $date.sql.txz $date.sql
-# Add a "-r your@email.address" for each key you want to encrypt to. Short or Long ID's should work too.
-/usr/bin/gpg2 --homedir /root/.gnupg --batch --yes -a --passphrase-file /root/.backups.conf -r kahn@the-mesh.org --sign --encrypt --trust-model always $date.sql.txz
-checksum="$(/usr/bin/sha512sum $date.sql*)"
-# Copy the encrypted archive to temp so mail can get to it.
-cp $date.sql.txz.asc /tmp/
-chmod 0444 /tmp/$date.sql.txz.asc
-# Cleanup files in the clear
-rm $date.sql
-rm $date.sql.txz
-rm $date.sql.txz.asc
-# Mail encrypted database to root user
-#echo "$checksum" | mail -s "MySQL Backup: $database" -a /tmp/$date.sql.txz.asc root
+# Backup function
+function backup {
+	# Backup databases
+	cd /root/
+	/usr/bin/mysqldump -u root $database > $date.sql
+	/bin/tar -cJf $date.sql.txz $date.sql
+	# Add a "-r your@email.address" for each key you want to encrypt to. Short or Long ID's should work too.
+	/usr/bin/gpg2 --homedir /root/.gnupg --batch --yes -a --passphrase-file /root/.backups.conf -r kahn@the-mesh.org --sign --encrypt --trust-model always $date.sql.txz
+	checksum="$(/usr/bin/sha512sum $date.sql*)"
+	# Copy the encrypted archive to temp so mail can get to it.
+	cp $date.sql.txz.asc /tmp/
+	chmod 0444 /tmp/$date.sql.txz.asc
+	# Cleanup files in the clear
+	rm $date.sql
+	rm $date.sql.txz
+	rm $date.sql.txz.asc
+	# Mail encrypted database to root user
+	#echo "$checksum" | mail -s "MySQL Backup: $database" -a /tmp/$date.sql.txz.asc root
 
-# Backup system files and include database backup
-time tar -cvJf /var/backup/charles.$date.txz / /tmp/$date.sql.txz.asc --exclude=/var/backup --exclude=/dev --exclude=/proc --exclude=/sys --exclude=/lost+found --exclude=/cgroup --exclude=/selinux --exclude=/tmp --exclude=/var/www/archive
-# Encrypt tar content
-/usr/bin/gpg2 --homedir /root/.gnupg --batch --yes -a --passphrase-file /root/.backups.conf -r kahn@the-mesh.org --sign --encrypt --trust-model always /var/backup/charles.$date.txz
+	# Backup system files and include database backup
+	time tar -cvJf /var/backup/charles.$date.txz / /tmp/$date.sql.txz.asc --exclude=/var/backup --exclude=/dev --exclude=/proc --exclude=/sys --exclude=/lost+found --exclude=/cgroup --exclude=/selinux --exclude=/tmp --exclude=/var/www/archive
+	# Encrypt tar content
+	/usr/bin/gpg2 --homedir /root/.gnupg --batch --yes -a --passphrase-file /root/.backups.conf -r kahn@the-mesh.org --sign --encrypt --trust-model always /var/backup/charles.$date.txz
+}
 
-# Offsite push - Update your details here for remote hosts
-/usr/bin/rsync -avz -e ssh /var/backups/charles.$date.txz.asc offsite@rsync.net:
+case $1 in
+daily)
+# Rotate dailys
+expiredDaily=`ssh $backuphost $backupkey -C find $remotedir/daily -type f -ctime $daily`
+for i in $expiredDaily; do
+echo 'Expired file' $i
+ssh $backuphost $backupkey -C rm -v $remotedir/daily/$i
+# Call backup
+backup
+rsync -avz -e ssh /var/backup/charles.$date.txz.asc $backuphost:$remotedir/daily/
+done
+;;
+weekly)
+# Rotate weeklys
+expiredWeekly=`ssh $backuphost $backupkey -C find $remotedir/weekly -type f -ctime $weekly`
+for i in $expiredWeekly; do
+echo 'Expired file' $i
+ssh $backuphost $backupkey -C rm -v $remotedir/weekly/$i
+# Call backup
+backup
+rsync -avz -e ssh /var/backup/charles.$date.txz.asc $backuphost:$remotedir/weekly/
+done
+;;
+monthly)
+# Rotate monthlys
+expiredMonthly=`ssh $backuphost $backupkey -C find $remotedir/monthly -type f -ctime $monthly`
+for i in $expiredMonthly; do
+echo 'Expired file' $i
+ssh $backuphost $backupkey -C rm -v $remotedir/monthly/$i
+# Call backup
+backup
+rsync -avz -e ssh /var/backup/charles.$date.txz.asc $backuphost:$remotedir/monthly/
+done
+;;
+*)
+echo "Notice: Script has been called without a time, not rotating backups"
+backup
+rsync -avz -e ssh /var/backup/charles.$date.txz.asc $backuphost:$remotedir/
+;;
+esac
 
 # Cleanup encrypted files
 rm /tmp/$date.sql.txz.asc
